0b06b35138ec8395c110b317e749cab5b3b49508
Francis Begyn <francis@begyn.be>
2022-09-17 22:52:14 +0200

    update to new nfpm layout

--- package.sh
+++ package.sh
@@ -1,9 +1,11 @@
-#!/bin/env bash
+#! /usr/bin/env bash
 
 ./fetch.sh $1 $2
 
 sed "s/vx.y.x/v$1/g" ./nfpm.yaml > ./nfpm-temp.yaml
 
-nfpm pkg --config="nfpm-temp.yaml" --target="node-exporter_$1_$2.deb"
+nfpm pkg --packager deb --config="nfpm-temp.yaml"
+nfpm pkg --packager rpm --config="nfpm-temp.yaml"
 
-rm -rf ./bin ./node_exporter-$1.linux-$2 ./node_exporter-$1.linux-$2.tar.gz ./nfpm-temp.yaml
+rm -rf ./node_exporter-$1.linux-$2 ./node_exporter-$1.linux-$2.tar.gz \
+	./nfpm-temp.yaml ./LICENSE ./NOTICE ./node_exporter
